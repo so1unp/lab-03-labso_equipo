0a4e70f1c462ac760479f4bd216da98aadb3ebb7
mae2033 <matiasaepullan@lgmail.com>
2025-04-14 13:57:57 +0000

    implementacion 1er version

--- sigcatch.c
+++ sigcatch.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+#include <string.h>
+//
 
+void handler(int sig)
+{
+    printf("%d: %s\n", sig, strsignal(sig));
+}
 int main(int argc, char *argv[])
 {
+    // refactoring: implementar con sigaction
+    signal(8, handler);
+    signal(15, handler);
+
+    while (1) {
+        pause();  // espera señales infinitamente
+    }
+
     exit(EXIT_SUCCESS);
 }
