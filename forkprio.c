bb3461a3eb8a5a7ff132941922763320a83be1ef
mae2033 <matiasaepullan@lgmail.com>
2025-04-23 15:49:03 +0000

    _NSIG

--- forkprio.c
+++ forkprio.c
@@ -2,15 +2,36 @@
 #include <stdlib.h>
 #include <sys/times.h>      // times()
 
+#define SEGUNDOS 30
+
 int busywork(void)
 {
     struct tms buf;
-    for (;;) {
+    for (// site_t i=0
+        ;// 0 < n 
+        ;// i++
+    ) {
         times(&buf);
     }
 }
 
+// crear un n procesos hijos, con prioridad progresivamente mas baja
 int main(int argc, char *argv[])
 {
+    // arg 1: n hijos a crear
+    // arg 2: seg. de ejecucion, 0 indefinido
+    // arg 3: si reducen las prioridades (1?)
+
+    // usar
+    // fork() crear un proceso
+    // setpriority() / nice() | getpriority()
+    // sleep()
+    // kill()
+    // sigaction(SIGTERM, handler, null)    
+    while(SEGUNDOS){
+
+    }
+    //Luego de un cierto número de segundos se debe envíar una señal de terminación (SIGTERM) a todos los procesos hijos.
+    // solucion alarm(cant_seg)
     exit(EXIT_SUCCESS);
 }
--- sigcatch.c
+++ sigcatch.c
@@ -5,22 +5,18 @@ void handler(int sig)
     printf("%d: %s\n", sig, strsignal(sig));
 }
 
-// int main(int argc, char *argv[])
-int main()
+int main(int argc, char *argv[])
 {
-    // refactoring: implementar con sigaction
-    // signal(SIGTERM, handler);
-    // signal(SIGFPE, handler);
     struct sigaction sa;
     sa.sa_handler = handler;
 
-
-    // usar siguientes lineas de comando para probar: 
+    // usar siguientes lineas para probar:
     //  kill -8 $(ps | grep sigcatch | awk '{print $1}')
     //  kill -15 $(ps | grep sigcatch | awk '{print $1}')
-    sigaction(SIGTERM, &sa, NULL);
-    sigaction(SIGFPE, &sa, NULL); 
-    // averiguar porque null
+    for (size_t i = 0; i < _NSIG; i++)
+    {
+        sigaction(i, &sa, NULL);
+    }
 
     while (1)
     {
