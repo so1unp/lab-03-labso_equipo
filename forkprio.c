ad4276ff8ea0de1f9be0b73f42438bbf0fcbaecd
mae2033 <matiasaepullan@lgmail.com>
2025-04-24 10:25:43 +0000

    NSIG

--- forkprio.c
+++ forkprio.c
@@ -1,18 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/times.h>      // times()
+#include <sys/times.h> // times()
+#include <signal.h>
 
-#define SEGUNDOS 30
+#define SEGUNDOS 3
 
 int busywork(void)
 {
     struct tms buf;
-    for (// site_t i=0
-        ;// 0 < n 
-        ;// i++
-    ) {
+    for ( // site_t i=0
+        ; // 0 < n
+        ; // i++
+    )
+    {
         times(&buf);
     }
+}
+void catch_alarm(int sig_num)
+{
+    printf("\nDemoraste demasiado!. Me estoy yendo...\n");
+    exit(0);
 }
 
 // crear un n procesos hijos, con prioridad progresivamente mas baja
@@ -21,17 +28,25 @@ int main(int argc, char *argv[])
     // arg 1: n hijos a crear
     // arg 2: seg. de ejecucion, 0 indefinido
     // arg 3: si reducen las prioridades (1?)
+    if (argc - 1 != 3)
+    {
+        exit(EXIT_FAILURE);
+    }
 
     // usar
     // fork() crear un proceso
     // setpriority() / nice() | getpriority()
     // sleep()
     // kill()
-    // sigaction(SIGTERM, handler, null)    
-    while(SEGUNDOS){
 
-    }
-    //Luego de un cierto número de segundos se debe envíar una señal de terminación (SIGTERM) a todos los procesos hijos.
-    // solucion alarm(cant_seg)
+    struct sigaction sa;
+    sa.sa_handler = catch_alarm;
+    sigaction(SIGALRM, &sa, NULL);
+    alarm(atoi(argv[2]));
+
+    // printf("Child %d (nice %2d):\t%3li\n");
+    pause();
+    // Luego de un cierto número de segundos se debe envíar una señal de terminación (SIGTERM) a todos los procesos hijos.
+    //  solucion alarm(cant_seg)
     exit(EXIT_SUCCESS);
 }
--- sigcatch.c
+++ sigcatch.c
@@ -13,10 +13,14 @@ int main(int argc, char *argv[])
     // usar siguientes lineas para probar:
     //  kill -8 $(ps | grep sigcatch | awk '{print $1}')
     //  kill -15 $(ps | grep sigcatch | awk '{print $1}')
-    for (size_t i = 0; i < _NSIG; i++)
+    for (int i = 1; i < _NSIG; i++)
     {
         sigaction(i, &sa, NULL);
     }
+    // los signals que no pueden se interceptados
+    // sigkill 9 y sigstop 19
+    // terminar proceso y detener procesos, 
+    // porque?
 
     while (1)
     {
